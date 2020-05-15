58e9e6ae5290d73f7214fc00fb6d3de6a1202a2e
Trung <bachbo2027@gmail.com>
2020-05-15 22:40:47 +0700

    add Left Shift Operator (<<)

--- QInt/QInt/main.cpp
+++ QInt/QInt/main.cpp
@@ -5,10 +5,16 @@ int main(int argc, char* argv[])
 	QInt a, b;
 	cout << "a: ";
 	cin >> a;
-	cout << "b: ";
-	cin >> b;
+	//cout << "b: ";
+	//cin >> b;
 	cout << "-a = " << a << "\n";
-	cout << "a + b = " << a + b << "\n";
-	cout << "a * b = " << a * b << "\n";
+	//cout << "a + b = " << a + b << "\n";
+	//cout << "a * b = " << a * b << "\n";
+	int amount;
+	cout << "amount: ";
+	cin >> amount;
+	cout << "a << " << amount << " = " << (a << amount) << "\n";
+	//cout << "b >> 1 = " << (b >> 0) << "\n";/
+
 	return 0;
 }
